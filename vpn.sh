b00548835760baafd297097be50e9638982087e5
Andrey Abramov <a.abramov@rbkmoney.com>
2021-10-09 00:33:28 +0300

    Add hints to shortcuts script

--- vpn.sh
+++ vpn.sh
@@ -9,6 +9,6 @@ case $1 in
     ssh root@`terraform show -json | jq '.values.root_module.resources[] | select (.name == "simple-wg-vpn") | .values.ip_address' | sed 's/"//g'` ;;
   "recreate" )
     terraform destroy -auto-approve && terraform apply -auto-approve ;;
-  *) echo >&2 "Invalid option: $@"; exit 1;;
+  *) echo >&2 "Invalid option: $@. Valid options are: apply, destroy, recreate, ssh"; exit 1;;
 esac
 
